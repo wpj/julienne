20e36ab3a8bed01ebb3156f7b98cbaa40627b2cc
Wyatt Johnston <w@wyatt.page>
2020-09-26 10:48:45 -0400

    Add tests for Site.createPage, Site.createResource, & Site.copyResource.

--- core/julienne/src/resource.ts
+++ core/julienne/src/resource.ts
@@ -1,18 +1,21 @@
 import type { Readable } from 'stream';
 
+export type FileResource = {
+  type: 'file';
+  from: string;
+};
+
+export type GeneratedResource = {
+  type: 'generated';
+  data: string | Buffer;
+};
+
+export type StreamResource = {
+  type: 'stream';
+  data: Readable;
+};
+
 /**
  * A resource destined to be written to the output directory.
  */
-export type Resource =
-  | {
-      type: 'file';
-      from: string;
-    }
-  | {
-      type: 'stream';
-      data: Readable;
-    }
-  | {
-      type: 'generated';
-      data: string | Buffer;
-    };
+export type Resource = FileResource | GeneratedResource | StreamResource;
